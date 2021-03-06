f4b29e4d6471a65c3dff567f99dbb233c035d018
williambayliss <wmbayliss@gmail.com>
2021-03-06 17:47:14 -0500

    Changed structure of adjacency list to be a list of Squares, each of which has an attribute called knight_moves, which will be a list of Squares that a knight can move to from that square

--- knights-travails.rb
+++ knights-travails.rb
@@ -10,36 +10,102 @@ class Board
                   [6,0], [6,1], [6,2], [6,3], [6,4], [6,5], [6,6], [6,7], 
                   [7,0], [7,1], [7,2], [7,3], [7,4], [7,5], [7,6], [7,7], 
                 ]
-    @matrix = []
-    chess_board.each do |start_square|
-      adjacent = []
-      chess_board.each do |end_square|
-        if end_square[0] == (start_square[0] + 2) && end_square[1] == (start_square[1] + 1)
-          adjacent.append(end_square)
-        elsif end_square[0] == (start_square[0] + 1) && end_square[1] == (start_square[1] + 2)
-          adjacent.append(end_square)
-        elsif end_square[0] == (start_square[0] - 1) && end_square[1] == (start_square[1] + 2)
-          adjacent.append(end_square)
-        elsif end_square[0] == (start_square[0] - 2) && end_square[1] == (start_square[1] - 1)
-          adjacent.append(end_square)
-        elsif end_square[0] == (start_square[0] - 1) && end_square[1] == (start_square[1] - 2)
-          adjacent.append(end_square)
-        elsif end_square[0] == (start_square[0] + 1) && end_square[1] == (start_square[1] - 2)
-          adjacent.append(end_square)
-        elsif end_square[0] == (start_square[0] + 2) && end_square[1] == (start_square[1] -1)
-          adjacent.append(end_square)
-        end
-      end
-      @matrix.append(adjacent)
+    @moves = create_adjacency_list(chess_board)
+
+    @moves.each do |adjency_list|
+      p adjency_list
     end
   end
 
-  def knight_moves start_coord, end_coord
-    knight = Knight.new
+  def knight_moves start_coord, end_coord, chess_board
+    start_square = Square.new(start_coord)
+    end_square = Square.new(end_coord)
+
+    start_index = find_square_index_in_adj_list(chess_board, start_coord)
+    
+    knight = Knight.new(start_square)
+
+    
+
+  end
+
+  def depth_first_search index, square
+    visited = []
+    if index.contains square
+      visited.append(square)
+    end
+
+
+    
+  end
+
+  # Takes Chess board and, for each square, iterates over the board to see if other squares on the board constitute a legal move for a Knight from that square
+  # If a Knight can move to the end_square from the start_square, a Square object is created with the x and y coordinates
+  # and added to the start_square's adjancy list.
+  # the full adjacency list - matrix - is returned at the end
+  def create_adjacency_list chess_board
+    adjency_list = []
+    chess_board.each do |start_square|
+      square = Square.new(start_square[0], start_square[1])
+      adjacent_squares = []
+      chess_board.each do |end_square|
+        if end_square[0] == (start_square[0] + 2) && end_square[1] == (start_square[1] + 1)
+          adjacent_square = Square.new(end_square[0], end_square[1])
+          adjacent_squares.append(adjacent_square)
+        elsif end_square[0] == (start_square[0] + 1) && end_square[1] == (start_square[1] + 2)
+          adjacent_square = Square.new(end_square[0], end_square[1])
+          adjacent_squares.append(adjacent_square)
+        elsif end_square[0] == (start_square[0] - 1) && end_square[1] == (start_square[1] + 2)
+          adjacent_square = Square.new(end_square[0], end_square[1])
+          adjacent_squares.append(adjacent_square)
+        elsif end_square[0] == (start_square[0] - 2) && end_square[1] == (start_square[1] - 1)
+          adjacent_square = Square.new(end_square[0], end_square[1])
+          adjacent_squares.append(adjacent_square)
+        elsif end_square[0] == (start_square[0] - 1) && end_square[1] == (start_square[1] - 2)
+          adjacent_square = Square.new(end_square[0], end_square[1])
+          adjacent_squares.append(adjacent_square)
+        elsif end_square[0] == (start_square[0] + 1) && end_square[1] == (start_square[1] - 2)
+          adjacent_square = Square.new(end_square[0], end_square[1])
+          adjacent_squares.append(adjacent_square)
+        elsif end_square[0] == (start_square[0] + 2) && end_square[1] == (start_square[1] -1)
+          adjacent_square = Square.new(end_square[0], end_square[1])
+          adjacent_squares.append(adjacent_square)
+        end
+      end
+      square.knight_moves = adjacent_squares
+      adjency_list.append(square)
+    end
+    adjency_list
+  end
+
+  def find_square_index_in_adj_list chess_board, coordinate
+    chess_board.each_with_index do |square, index|
+      if square == coordinate
+        return index
+      end
+    end
+  end
 end
 
+# Object placed into adjacency lists, with data for x and y coordinate
+class Square
+  attr_accessor :x_coord, :y_coord, :knight_moves
+  def initialize x_coord, y_coord
+    @x = x_coord
+    @y = y_coord
+    @knight_moves = nil
+  end
+end
+
+# Object that takes a Square as its current position, and contains a method to update the Square to a new position
 class Knight
-  def initialize
+    attr_accessor :square
+  def initialize square
+    @square = square
+  end
+
+  def move coordinate
+    @square = coordinate
   end
 end
 
