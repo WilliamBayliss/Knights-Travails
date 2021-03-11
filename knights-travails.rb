ceb5a82e101ebe15d5f31953420e560ba309aa51
williambayliss <wmbayliss@gmail.com>
2021-03-11 00:41:51 -0500

    Finished BFS algorithm, implemented logic to go backwards from endpoint through path found from BFS and return Knights Path. Implemented functions to get and sanitize user input. Added puts to program launch detailing instructions to user

--- knights-travails.rb
+++ knights-travails.rb
@@ -1,9 +1,10 @@
 class KnightsTravails
   def initialize
+    puts "Welcome to Knights Travails! This program will tell you the shortest path for a Knight from one square on a chess board to any other square on the board."
+    puts "You will be prompted for a coordinate in the form [X,Y], please use digits from 0-7 to represent the row/column of the square"
+    puts "Example, the first square on the board would be [0,0], and the last square would be [7,7]"
     board = Board.new
   end
-
-
 end
 
 # Creates a Graph structure for squares on a chess board
@@ -12,35 +13,97 @@ class Board
   def initialize
     @board = {}
 
+    # Creates Graph and adds edges to squares if a Knight can move from one
+    # to another
     create_graph(get_chess_board_array)
     add_edges_to_graph()
-    @board.each do |key, value|
-      if value.knight_moves.length > 0
-        puts value.knight_moves.length
-      end
-    end
+
+    knight_moves()
+
+    
   end
 
   def add_square(square)
     @board[square.coordinate] = square
   end
 
-  def add_edge(predecessor_coordinate, successor_coordinate)
-    @board[predecessor_coordinate].add_edge(@board[successor_coordinate])
+  def add_edge(predecessor, successor)
+    @board[predecessor].add_edge(@board[successor])
   end
 
   def [](coordinate)
     @board[coordinate]
   end
 
-  def find_path
-    
+  def knight_moves
+    # Prompts user to provide coordinates for start and end position,
+    # Sanitizes those inputs and returns 2 item array of corresponding squares from the board
+    start_and_finish = get_squares()
+    # Runs BFS algorithm on graph to create path from start square to all other squares
+    # possible_paths is array of hashes where index corresponds to Square index, with attributes of distance and predecessor
+    pathway = find_path(start_and_finish[0])
+
+
+    knight = Knight.new
+    current_point = pathway[start_and_finish[1].index]
+    knight.move(start_and_finish[1])
+
+    until current_point[:predecessor].nil?
+      knight.move(current_point[:predecessor])
+      current_point = pathway[current_point[:predecessor].index]
+    end
+    knight.path.reverse!
+
+    puts "Your Knight took #{knight.path.length} moves! Here's Your path:"
+    knight.path.each do |step|
+      puts "Knight to #{step.coordinate}"
+    end
+  end
+
+  def find_path source
+    square_info = []
+
+    # Creates an Array of Hashes, square_info, where the index in the array
+    # corresponds to the index of a Square on the board,
+    # giving each hash at the index properties distance and predecessor
+    @board.each_with_index do |(key, value), index|
+      square_info[index] = {
+        distance: nil,
+        predecessor: nil
+      }
+    end
+
+    # Set source distance to 0 to mark as visited
+    square_info[source.index][:distance] = 0
+
+    # Create queue and add source Square
+    queue = []
+    queue.append(source)
+
+    while queue.length() > 0
+      # Dequeue Square
+      temp = queue[0]
+      queue.shift
+
+      # For each neighbour vertex of Square that has not been visited
+      temp.knight_moves.each do |neighbour|
+        if square_info[neighbour.index][:distance].nil?
+          # Set distance to Square.distance + 1 and set predecessor to Square
+          square_info[neighbour.index][:distance] = square_info[temp.index][:distance] + 1
+          square_info[neighbour.index][:predecessor] = temp
+          # Enqueue Square's neighbouring vertices
+          queue.append(neighbour)
+        end
+      end
+    end
+    # Return 
+    square_info
   end
 
 
   def create_graph chess_board
-    chess_board.each do |coordinate|
-      square = Square.new(coordinate)
+    chess_board.each_with_index do |coordinate, index|
+      square = Square.new(coordinate, index)
       add_square(square)
     end
   end
@@ -89,13 +152,58 @@ class Board
     return chess_board
   end
 
+  def get_squares
+    puts "Where would you like your Knight to begin?"
+    start_coordinate = get_coord_from_user()
+    until (start_coordinate[0] >= 0 && start_coordinate[0] < 8) && 
+          (start_coordinate[1] >= 0 && start_coordinate[1] < 8)
+      puts "Invalid input"
+      start_coordinate = get_coord_from_user  
+    end
+
+    start_square = @board[start_coordinate]
+    puts "Where would you like them to go?"
+    
+    end_coordinate = get_coord_from_user()
+    until (end_coordinate[0] >= 0 && end_coordinate[0] < 8) && 
+          (end_coordinate[1] >= 0 && end_coordinate[1] < 8)
+      puts "Invalid input"
+      end_coordinate = get_coord_from_user  
+    end
+    end_square = @board[end_coordinate]
+    
+    array = [start_square, end_square]
+    array
+  end
+  
+  def get_coord_from_user
+    puts "Please enter a coordinate in the form X,Y"
+    input = gets.chomp
+    coordinate = sanitize_input(input)
+    coordinate
+  end
+
+  # Will remove any char that isn't a digit from user input, and ensure the
+  # coordinate only takes the first 2 digits if the user enters more than 2
+  def sanitize_input input
+    input = input.split("")
+    array = []
+    input.each do |char|
+      if char =~ /\d/
+        array.append(char.to_i)
+      end
+    end
+    return array[0..1]
+  end
+
 end
 
 # Object placed into adjacency lists, with data for x and y coordinate
 class Square
-  attr_reader :coordinate, :knight_moves
-  def initialize coordinate
+  attr_reader :coordinate, :index, :knight_moves
+  def initialize coordinate, index
     @coordinate = coordinate
+    @index = index
     @knight_moves = []
   end
 
@@ -106,13 +214,13 @@ end
 
 # Object that takes a Square as its current position, and contains a method to update the Square to a new position
 class Knight
-    attr_accessor :square
-  def initialize square
-    @square = square
+    attr_accessor :path
+  def initialize
+    @path = []
   end
 
   def move new_position
-    @square = new_position
+    @path << new_position
   end
 end
 
